76e2e9294f71c361853599d1a6731e4c6e67a438
rudraksh121a <rudrakshkanungo2022@gmail.com>
2026-01-23 02:07:31 +0530

    addmovie add

--- Server/MovieServer/controllers/movie_contraller.go
+++ Server/MovieServer/controllers/movie_contraller.go
@@ -1,10 +1,85 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Rudraksh121a/Movie-Streaming-web/database"
+	"github.com/Rudraksh121a/Movie-Streaming-web/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var movieCollection *mongo.Collection = database.OpenCollection("Movies")
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "List of movies"})
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movies []models.Movie
+		cursor, err := movieCollection.Find(ctx, bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to fetch movies"})
+			return
+		}
+		defer cursor.Close(ctx)
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to decode movies"})
+			return
+		}
+
+		c.JSON(http.StatusOK, movies)
+	}
+}
+
+func GetMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		movidId := c.Param("imdb_id")
+		if movidId == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie Id is Required"})
+			return
+		}
+		var movie models.Movie
+
+		err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movidId}).Decode(&movie)
+		if err != nil {
+
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie Not Found"})
+		}
+		c.JSON(http.StatusOK, movie)
+
+	}
+}
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+		if err := c.ShouldBindBodyWithJSON(&movie); err != nil {
+
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+			return
+		}
+		if err := validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "validation failed", "detail": err.Error()})
+			return
+		}
+
+		result, err := movieCollection.InsertOne(ctx, movie)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to add movie"})
+			return
+		}
+		c.JSON(http.StatusCreated, result)
 	}
 }
--- Server/MovieServer/database/database_connection.go
+++ Server/MovieServer/database/database_connection.go
@@ -1,8 +1,50 @@
 package database
 
-import "go.mongodb.org/mongo-driver/v2/mongo"
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
 
-// func DBInstance()*mongo.Client{
+func DBInstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Unable to find .env")
+	}
 
-// }
+	MongoDb_URI := os.Getenv("MONGO_URI")
+	if MongoDb_URI == "" {
+		log.Fatal("MongoDB URI not set")
+	}
+	fmt.Println("MonogoDB URI", MongoDb_URI)
+
+	clientOption := options.Client().ApplyURI(MongoDb_URI)
+	client, err := mongo.Connect(clientOption)
+	if err != nil {
+		return nil
+	}
+
+	return client
+
+}
+
+var client *mongo.Client = DBInstance()
+
+func OpenCollection(collectionName string) *mongo.Collection {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("MongoDB URI not set")
+	}
+	databaseName := os.Getenv("DATABASE_NAME")
+	fmt.Println("Database Name :", databaseName)
+
+	collection := client.Database(databaseName).Collection(collectionName)
+	if collection == nil {
+		return  nil
+	}
+	return collection
+}
